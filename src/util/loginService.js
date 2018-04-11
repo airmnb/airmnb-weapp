3547a617c7907ea44d64aecbb26f942190759f3a
Jun Shao <mr.shaojun@gmail.com>
2018-04-12 00:36:14 +1000

    UI tweaks

--- src/util/loginService.js
+++ src/util/loginService.js
@@ -3,14 +3,18 @@ import wepy from 'wepy';
 
 class LoginService {
   async login() {
-		const resp = await wepy.login();
-		const code = resp.code;
-		console.log('login code: ', code);
+    try {
+      await wepy.checkSession();
+    }catch(e) {
+      const resp = await wepy.login();
+      const code = resp.code;
+      console.log('login code: ', code);
 
-    const profile = await restClient.get('/auth/wechat/login', {code});
-		console.log('login profile: ', profile);
-    
-    return profile;
+      const profile = await restClient.get('/auth/wechat/login', {code});
+      console.log('login profile: ', profile);
+      
+      return profile;
+    }
   }
 
   async getWechatUserInfo() {
