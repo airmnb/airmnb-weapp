ce518a1f050da80648439165d00648e596d37579
Jun Shao <mr.shaojun@gmail.com>
2018-04-28 00:09:03 +1000

    Move translation strings to i18n.js file

---
+++ src/util/i18n.js
@@ -0,0 +1,29 @@
+const i18n = {
+  "en": {
+    "map_search_bar": "Search",
+    "stat_refresh": "Refresh",
+    "stat_users": "Users",
+    "stat_babies": "Babies",
+    "stat_providers": "Providers",
+    "stat_activities": "Activities",
+    "stat_bookings": "Bookings",
+    "stat_transactions": "Transactions",
+    "stat_title": "Statistic",
+    "map_title": "Air Mom & Baby"
+  },
+  "zh_CN": {
+    "map_search_bar": "搜  索",
+    "stat_refresh": "刷  新",
+    "stat_users": "登录用户",
+    "stat_babies": "登录的孩子",
+    "stat_providers": "活动提供者（团体或个人）",
+    "stat_activities": "创建的活动",
+    "stat_bookings": "预约的活动",
+    "stat_transactions": "成交的活动",
+    "stat_title": "爱彼儿 - 统计数据",
+    "map_title": "爱彼儿"
+  },
+  "zh_TW": {}
+}
+
+export default i18n;
