672dbabd123ce5ce9a7c91afbf9f30ca29fd36fd
Jun Shao <mr.shaojun@gmail.com>
2018-05-07 23:23:17 +1000

    Able to add baby

--- src/util/babyService.js
+++ src/util/babyService.js
@@ -1,15 +1,19 @@
 import {apiClient} from "./restClient";
+import amb from "@/util/amb";
 
 class BabyService {
   async get(id){
     return await apiClient.get(`babies/${id}`);
   }
 
-  async save(baby) {
+  async add(baby) {
+    baby = amb.cleanSetModel(baby);
+    baby.creatorId = amb.data.user.userId;
     await apiClient.post('babies', baby);
   }
 
-  async upsert(baby) {
+  async update(baby) {
+    baby = amb.cleanSetModel(baby);
     await apiClient.put('babies', baby);
   }
 }
