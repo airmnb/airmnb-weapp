16863d658845ad74c3f4b2aa7886497fdfcd137a
Jun Shao <mr.shaojun@gmail.com>
2018-07-19 22:11:54 +1000

    Initial implementation of image compression before uploading

--- src/util/imageService.js
+++ src/util/imageService.js
@@ -9,12 +9,15 @@ class ImageService {
   }
 
   async chooseImage(count) {
+    console.log('choose');
     const res = await wepy.chooseImage({
       count: count || 1,
       sizeType: ['compressed'],
     });
     wepy.showLoading({mask: true});
-    const tasks = res.tempFilePaths.map(f => this.uploadSingleFilePromise(f));
+    const tasks = res.tempFilePaths
+      .map(f => this.compressImageWithCanvas(f, 'imageCompressCanvas'))
+      .map(p => this.uploadSingleFilePromise(p));
     const resps = await Promise.all(tasks);
     console.log(resps);
     wepy.hideLoading();
@@ -31,18 +34,51 @@ class ImageService {
     return ids;
   }
 
-  uploadSingleFilePromise(localPath) {
+  async compressImageWithCanvas(bigImagePath, canvasId) {
+    const width = 360;
+    const height = 240;
+    const info = await wepy.getImageInfo({
+      src: bigImagePath
+    })
+    // return bigImagePath;
+    if(info.width <= width && info.height <= height) {
+      // Return if the image is small enough.
+      return bigImagePath;
+    }
+
+    const ctx = wx.createCanvasContext(canvasId);
+    ctx.drawImage(bigImagePath, 0, 0, width, height);
+    return new Promise((res, rej) => {
+      ctx.draw(true, async (e) => {
+        try{
+          const result = await wepy.canvasToTempFilePath({
+            canvasId
+          });
+          const smallPath = result.tempFilePath;
+          res(smallPath);
+        }catch(e) {
+          rej(e);
+        }
+      });
+    });
+  }
+
+  async uploadSingleFilePromise(smallPathPromise) {
     const authHeader = apiClient.getRequestHeaders();
     const header = Object.assign(authHeader, {
       'Content-Type': 'multipart/form-data'
     });
-    return wepy.uploadFile({
+    const path = await smallPathPromise;
+    console.log('small iamge', path);
+    return await wepy.uploadFile({
       url: apiClient.path2Url('/images'),
       header: header,
-      filePath: localPath,
+      filePath: path,
       name: 'dataFile'}
     )
   }
+
+  
 }
 const imageService = new ImageService();
 export default imageService;
